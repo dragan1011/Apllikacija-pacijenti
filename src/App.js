e9568a0c5392a7c9c425d375320bb5837ca8d6a1
Dragan Rapić <82722626+dragan1011@users.noreply.github.com>
2022-09-01 23:26:43 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import { useEffect, useState } from "react";
 import './App.css';
 import Modal from './components/Modal.js'
 import Pacijenti from "./components/Pacijenti";
-import Edit from './components/Edit'
 
 function App() {
-
 
 const [items, setItems] = useState([]);
 const [isSearching, setIsSearching] =useState(false);
@@ -36,18 +34,20 @@ useEffect(() => {
 }, []);
 
 
-const handleNameSearch = async (e) =>{
-
+ function search(e) {
+e.preventDefault();
+if (nameValue.length >= 1 ) {
+ 
   e.preventDefault();
 
-  if (nameValue.trim() ==  '' || nameValue.trim() == null) {
+  if (nameValue.trim() ===  '' || nameValue.trim() === null) {
     return alert('Morate unijeti karaktere za pretragu!');
   }
   if (nameValue.trim().length >= 1 && nameValue.trim().length <= 3) {
     return alert('Morate unijeti više od tri karaktera za pretragu!')
   }
   
-  return await axios
+  return axios
   .get(`http://81.93.66.18:8234//api2.cfc?method=pacijent_trazi&ime=${nameValue}`)
   .then((response)=> {
     console.log(response.data.lista_pacijenata.DATA);
@@ -63,25 +63,20 @@ const handleNameSearch = async (e) =>{
       }
     });
     setData(transformedData);
-    setNameValue("");
     setIsSearching(true);
   })
   .catch((err) => console.log(err))
-  }
-
- 
-
-const handleJMBGSearch = async (e) =>{
+}else{
   e.preventDefault();
 
-  if (jmbgValue.trim() ==  '' || jmbgValue.trim() == null) {
+  if (jmbgValue.trim() ===  '' || jmbgValue.trim() === null) {
     return alert('Morate unijeti karaktere za pretragu!');
   }
   if (jmbgValue.trim().length >= 1 && jmbgValue.trim().length <= 3) {
     return alert('Morate unijeti više od tri karaktera za pretragu!')
   }
   
-  return await axios
+  return axios
   .get(`http://81.93.66.18:8234//api2.cfc?method=pacijent_trazi&jmbg=${jmbgValue}`)
   .then((response)=> {
     console.log(response.data.lista_pacijenata.DATA);
@@ -97,31 +92,18 @@ const handleJMBGSearch = async (e) =>{
       }
     });
     setData(transformedData);
-    setJMBGValue("");
     setIsSearching(true);
   })
   .catch((err) => console.log(err))
 }
-
-
-
-console.log(nameValue.length)
-
-/* 
- function search(e) {
- if (nameValue.length > 0) {
-  handleNameSearch();
- }else{
-  handleJMBGSearch();
- }
 }  
- */
   return (
+    
     <div className="App">
       <header className="App-header">
       <div className="search-container">
         <div className="patients">Evidencija pacijenata</div>
-        <form onSubmit={handleNameSearch}>
+        <form onSubmit={search}>
         <input  type="number" onChange={(e) => setJMBGValue(e.target.value)} className="search" placeholder="JMBG..." />
         <input value={nameValue} type="text" onChange={(e) => setNameValue(e.target.value)} className="search" placeholder="Ime ili prezime..." />
           <button type="submit" className="search-trazi" >Traži</button>
@@ -131,8 +113,10 @@ console.log(nameValue.length)
           </div>
           {isSearching && <Pacijenti pacijenti={data}/>}
                </header>
-      
+               
     </div>
+
+ 
   );
 }
 
--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React from 'react';
 import './Pacijenti.css';
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 import axios from "axios";
+import './Edit.css'
 
 function Edit(props) {
 
@@ -9,6 +10,34 @@ function Edit(props) {
     const imeRef = useRef();
     const prezimeRef = useRef();
     const jmbgRef = useRef();
+
+    const [data, setData] = useState({
+        grad:''
+    })
+    
+    const [items, setItems] = useState([]);
+
+    const fetchGradovi = async () => {
+        const response = await fetch("http://81.93.66.18:8234//api.cfc?method=gradovi_lista");
+        const data = await response.json();
+      
+        const transformedData = data.gradovi.DATA.map(item => {
+          
+          return {
+            id_grad: item[0],
+            naziv: item[1]
+          }
+        });
+        setItems(transformedData);
+      }
+      
+      useEffect(() => {
+       fetchGradovi();
+      }, []);
+      
+
+      console.log(items)
+      
 
 
  function submit(e) {
@@ -35,17 +64,28 @@ function Edit(props) {
     console.log(imeRef.current.value);
     console.log(prezimeRef.current.value);
     console.log(jmbgRef.current.value);
-            const url = `http://81.93.66.18:8234/api2.cfc?method=pacijent_unos&ime=${imeRef.current.value}&prezime=${prezimeRef.current.value}&jmbg=${jmbgRef.current.value}&id_grad=2&id=${props.podaci.id}`;
+            const url = `http://81.93.66.18:8234/api2.cfc?method=pacijent_unos&ime=${imeRef.current.value}&prezime=${prezimeRef.current.value}&jmbg=${jmbgRef.current.value}&id_grad=${+data.grad}&id=${props.podaci.id}`;
 
     axios.post(url, {
         ime: imeRef.current.value,
         prezime: prezimeRef.current.value,
         jmbg: jmbgRef.current.value,
+        id_grad: +data.grad
     })
     .then(res=> {      
         alert('Uspješno ste napravili izmjene!');
+        props.closeModal(false)
     })  
 }
+
+console.log(data.grad)
+
+function handle(e){
+    const index = e.target.children[e.target.selectedIndex].dataset.id;
+    const newdata={grad: index}
+    console.log(newdata)
+    setData(newdata)
+   }
 
 return (
 
@@ -55,16 +95,23 @@ return (
     <span className="close"  onClick={() => props.closeModal(false)}  >✖</span></div>
     <span className="title">Izmjena podataka pacijenta</span>
 <div className="name">
-    <input type="text" id="firstName" ref={imeRef} defaultValue={props.podaci.ime}  />
+    <input type="text" id="firstName" ref={imeRef} defaultValue={props.podaci.ime.charAt(0).toUpperCase() + props.podaci.ime.slice(1).toLowerCase()}  />
  </div>
  <div className="name">
-     <input type="text" id="lastName"  ref={prezimeRef} defaultValue={props.podaci.prezime}  />
+     <input type="text" id="lastName"  ref={prezimeRef} defaultValue={props.podaci.prezime.charAt(0).toUpperCase() + props.podaci.prezime.slice(1).toLowerCase()}  />
       
  </div>
  <div className="name jmbg">
      <input type="number" id="jmbg" ref={jmbgRef} defaultValue={props.podaci.jmbg}  />
  </div>
- <div className="name jmbg">
+ <div className="grad">
+ <select className="gradovi" onChange={handle} >
+                    {items.map(item => (
+                      <option className="listaGradova" data-id={item.id_grad} > 
+                        { item.naziv }   
+                        </option> 
+                     ))}
+                </select>
  </div>
  <input type="submit" className="add"  value="Sacuvaj" />
  <input type="button" className="exit" onClick={() => props.closeModal(false)}  value="Odustani" />
